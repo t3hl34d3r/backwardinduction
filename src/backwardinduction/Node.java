fc1e768a6cdd9384a05fee2ed6e83efdcf807830
Marton Czwick <martonczwick@gmail.com>
2015-12-08 16:10:46 +0100

    Outcome is now showed on the leafs

--- src/backwardinduction/Node.java
+++ src/backwardinduction/Node.java
@@ -5,17 +5,20 @@ import java.util.LinkedList;
 public class Node {
 	//Akármilyen információ
 	private String name;	
-	private double value=1;
-	
-	private double b = -3;
-	private double p = 5;
 	
 	private double outcome[] = new double[4];
 	
 	@Override
 	public String toString(){
+		String label = name;
+		label += " ";
 		
-		return name;
+		for (int i = 0;i<4;i++){
+			label+=" ";
+			label+=Integer.toString((int)Math.round(outcome[i]));
+		}
+		
+		return label;
 	}
 	
 	Node(){
@@ -26,17 +29,6 @@ public class Node {
 		name = asd;
 	}
 	
-	Node(double n){
-		value = n;
-	}
-	
-	public double getValue(){
-		return value;
-	}
-	
-	public void setValue(int v){
-		value = v;
-	}
 	
 	public double[] getOutcome(){
 		return outcome;
